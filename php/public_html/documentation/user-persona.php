e785f75eba72ed564fa35d7150891186c146dbbe
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-24 14:31:27 -0600

    Added personas

---
+++ php/public_html/documentation/user-persona.php
@@ -0,0 +1,17 @@
+<!DOCTYPE=HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Persona</title>
+</head>
+<body>
+<h>Persona-Casual User</h>
+<p>Example: Jim is a casual user. Jim is a young adult currently living with his parents. Jim finished college, but has not been able to find a professional job in his field of study yet. He has a lot of free time, and spends most of it playing video games and working at a local restaurant in the evenings. He is curious about what crime is in his neighborhood, and how much the houses in his area are worth. He would like to use the site casually to look at how much houses cost so he can determine if it is really worth it to live in Albuquerque considering the crime level.  </p>
+<h>Persona-Registered User</h>
+<p>
+	Example: Sarah is a registered user. She is a woman in her mid-30s who just accepted a job in Albuquerque, and will be moving here from Denver. Sarah is a cautious person and sticks to her routines and is a self-proclaimed perfectionist. Sarah is a “workaholic” and attempts to use self-discipline strategies to keep her work-life balance in check, but usually ends up working more than she plans to. Sarah goes to the gym daily, and occasionally treats herself to a day at the spa.   Sarah has heard rumors about crime being rampant in Albuquerque, and is hoping to purchase a home in an area that is within her budget, but also has a low instance of crime. Sarah has found a few different homes she has been interested in, and would like to know what the price range is and the instance of crime for each home.
+</p>
+<h>Persona-Admin</h>
+<p>Example: Karen is a freelance tech worker. She enjoys creating websites that she believes will help out in her community. karen enjoys being outside, walking her dogs, and spending time alone in front of her fireplace with her laptop in hand. As an administrator for this site and will need to be able to log into the site and fix any errors that may come up.  </p>
+</body>
+</html>
