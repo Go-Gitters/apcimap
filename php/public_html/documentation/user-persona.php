4ec904ba0bdf4f85af972c4f06f82537f17f44d6
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-24 20:34:59 -0600

    finished headers

--- php/public_html/documentation/user-persona.php
+++ php/public_html/documentation/user-persona.php
@@ -12,16 +12,39 @@
 <h3>Career</h3>
 <p>Jim is a young adult currently living with his parents. Jim finished college, but has not been able to find a professional job in his field of study yet.<p>
 	<h3>Technology</h3>
+Jim still has the mac that he used in college a couple of years ago. He has added on a lot of "gadgets" as he calls them for gaming. Jim has the newest android phone which he is on constantly.
 <h3>Attitudes and Behaviors</h3>
-<p>Jim has a lot of free time, and spends most of it playing video games and working at a local restaurant in the evenings.
-	<h3>Goals</h3>
-Jim is curious about what crime is in his neighborhood, and how much the houses in his area are worth. He would like to use the site casually to look at how much houses cost so he can determine if it is really worth it to live in Albuquerque considering the crime level.  </p>
+<p>Jim has a lot of free time, and spends most of it playing video games and working at a local restaurant in the evenings.</p>
+<h3>Needs and Frustrations</h3>
+<p>Jim wants to get this information with minimal effort. He wants to casually browse the site for information, and does not want to be bothered with unnecessary extra steps or too much time searching.</p>
+<h3>Goals</h3>
+<p>Jim is curious about what crime is in his neighborhood, and how much the houses in his area are worth. He would like to use the site casually to look at how much houses cost so he can determine if it is really worth it to live in Albuquerque considering the crime level.</p>
 <h2>Persona-Registered User</h2>
 <h3>Name: Sarah</h3>
-
-<p>Sarah is a registered user. She is a woman in her mid-30s who just accepted a job in Albuquerque, and will be moving here from Denver. Sarah is a cautious person and sticks to her routines and is a self-proclaimed perfectionist. Sarah is a “workaholic” and attempts to use self-discipline strategies to keep her work-life balance in check, but usually ends up working more than she plans to. Sarah goes to the gym daily, and occasionally treats herself to a day at the spa.   Sarah has heard rumors about crime being rampant in Albuquerque, and is hoping to purchase a home in an area that is within her budget, but also has a low instance of crime. Sarah has found a few different homes she has been interested in, and would like to know what the price range is and the instance of crime for each home.
+<h3>Age:35</h3>
+<h3>Career</h3>
+<p> Sarah is a women in her mid-30s who just accepted a job in Albuquerque, and will be moving to Albuquerque from Denver. Sarah is a registered nurse, and works in a hospital. </p>
+<h3>Technology</h3>
+<p>Sarah uses a computer quite a bit at work, and occasionally uses a three year old desktop at home. Sarah uses an Iphone 8. She knows that this model is a bit outdated, but she still feels that it does what she needs it to. </p>
+<h3>Attitudes and Behavior</h3>
+<p>Sarah is a cautious person and sticks to her routines and is a self-proclaimed perfectionist. Sarah is a “workaholic” and attempts to use self-discipline strategies to keep her work-life balance in check, but usually ends up working more than she plans to. Sarah goes to the gym daily, and occasionally treats herself to a day at the spa. </p>
+<h3>Frustrations and Needs</h3>
+	<p> Sarah has heard rumors about crime being rampant in Albuquerque, and is hoping to purchase a home in an area that is within her budget, but also has a low instance of crime. Sarah has found a few different homes she has been interested in, and would like to know what the price range is and the instance of crime for each home.
 </p>
-<h>Persona-Admin</h>
-<p>Example: Karen is a freelance tech worker. She enjoys creating websites that she believes will help out in her community. karen enjoys being outside, walking her dogs, and spending time alone in front of her fireplace with her laptop in hand. As an administrator for this site and will need to be able to log into the site and fix any errors that may come up.  </p>
+<h3>Goals</h3>
+<p>Sarah would like to use this website to research potential homes to purchase in her new town of Albuquerque. She would like to be able to star the homes she is interested in for later reference as she maks her selection. </p>
+<h2>Persona-Admin</h2>
+<h3>Name: Karen</h3>
+<h3>Age: 37</h3>
+<h3>Career</h3>
+<p>Karen is a freelance tech worker. She enjoys creating websites that she believes will help out in her community, and fixing any bugs she sees in existing ones. </p>
+<h3>Technology</h3>
+<p>As a person in the tech industry, Karen uses a newer laptop with a docking station. She also has an android phone that she uses, but nearly as much as she uses her computer.</p>
+<h3>Attitudes and Behavior</h3>
+<p>Karen enjoys being outside, walking her dogs, and spending time alone in front of her fireplace with her laptop in hand.</p>
+<h3>Frustrations and Needs</h3>
+<p>Karen appreciates when a website is designed well. She becomes easily frustrated when she comes across code that seems messy or redundant. She likes to fix these errors and make the code more streamline and accessable. </p>
+<h3>Goals</h3>
+		<p>As an administrator for this site and will need to be able to access the code into and fix any errors that may come up. She may also need to occasionally view the data and determine if it needs to be updated. She will also be capable of updating it if needed. </p>
 </body>
 </html>
