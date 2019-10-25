3d3bf204c8b87e6353ebc0424b0a4199d3d37bf8
Kyla Bendt <kylabendt@gmail.com>
2019-10-25 09:13:37 -0600

    add wireframe images

---
+++ php/public_html/documentation/wireframes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>
+		Wireframes
+	</title>
+</head>
+<body>
+<h1>Wireframes</h1>
+<h2>Mobile</h2>
+<img src="images/mobile-wireframe-small.png">
+
+<h2>Web</h2>
+<img src="images/web-wireframe-small.png">
+</body>
+</html>
