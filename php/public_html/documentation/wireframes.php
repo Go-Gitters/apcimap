8a24ccc28863ac03ef265b44bd809b7396ac287a
Kyla Bendt <kylabendt@gmail.com>
2019-10-28 11:52:43 -0600

    add sign-up wireframe

--- php/public_html/documentation/wireframes.php
+++ php/public_html/documentation/wireframes.php
@@ -16,8 +16,10 @@
 
 <h2>Computer</h2>
 <h3>Map</h3>
-<p><img src="images/pc-map.png" style="width:600px"></p>
+<p><img src="images/pc-map.png" style="width:800px"></p>
 <h3>Sign-in</h3>
-<p><img src="images/pc-sign-in.png" style="width:600px"></p>
+<p><img src="images/pc-sign-in.png" style="width:800px"></p>
+<h3>Sign-up</h3>
+<p><img src="images/pc-sign-up.png" style="width:800px"></p>
 </body>
 </html>
