057f1669897a90e04a05c83ed8d56b9bb5c9c4cd
Lisa Lee <llee28@cnm.edu>
2019-10-28 15:44:03 -0600

    start identifying getfoobybar methods

---
+++ php/public_html/documentation/getfoobybars.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Identifying Needed getFooByBar Methods</title>
+
+</head>
+<body>
+	<h1>User</h1>
+		<ul>
+			<li>Email</li>
+			<li>Username</li>
+		</ul>
+
+</body>
+
+</html>
