d86e23b53f43b40a28d5df8dbc4a840e0cb31958
Kyla Bendt <kylabendt@gmail.com>
2019-10-29 13:22:46 -0600

    added boilerplate code for Class

--- php/Classes/Property.php
+++ php/Classes/Property.php
@@ -1,1 +1,18 @@
 <?php
+
+
+namespace GoGitters\ApciMap;
+
+/**
+ * This class represents a property and includes attributes like latitude, longitude & assessed values.
+ *
+ * @author Kyla Bendt
+ * @version 0.0.1
+ *
+ **/
+
+class Property {
+
+
+
+}
