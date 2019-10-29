e140870959f54e78587608b8a014cfc8f14f7f94
Kyla Bendt <kylabendt@gmail.com>
2019-10-29 13:36:02 -0600

    add outline for class

--- php/Classes/Property.php
+++ php/Classes/Property.php
@@ -10,9 +10,85 @@ namespace GoGitters\ApciMap;
  * @version 0.0.1
  *
  **/
+/*
+ * TODO Remove this before merging with dev!
+ * DDL for property table for reference
+ *
+ * CREATE TABLE property (
+	propertyUuid          BINARY(16)     NOT NULL,
+	propertyCity          VARCHAR(80)    NOT NULL,
+	propertyClass         CHAR(1)        NOT NULL,
+	propertyLatitude      DECIMAL(9, 6)  NOT NULL,
+	propertyLongitude     DECIMAL(9, 6)  NOT NULL,
+	propertyStreetAddress VARCHAR(134)   NOT NULL,
+	propertyValue         DECIMAL(15, 2) NOT NULL,
+	PRIMARY KEY (propertyUuid)
+)*/
 
 class Property {
+	//TODO write and document all state variables
+	/********************************************
+	 * Declare and document all state variables *
+	 ********************************************/
+	/*
+	 * TODO $propertyUuid
+	 */
+
+	/*
+	 * TODO $propertyCity
+	 */
+
+	/*
+	 * TODO $propertyLatitude
+	 */
+
+	/*
+	 * TODO $propertyLongitude
+	 */
+
+	/*
+	 * TODO $propertyStreetAddress
+	 */
+
+	/*
+	 * TODO $propertyValue
+	 */
+
+	/********************************************
+	 * TODO Constructor method                  *
+	 ********************************************/
+
+	/********************************************
+	 * TODO Getters and Setters                 *
+	 ********************************************/
+	/*
+	 * TODO $propertyUuid
+	 */
+
+	/*
+	 * TODO $propertyCity
+	 */
+
+	/*
+	 * TODO $propertyLatitude
+	 */
+
+	/*
+	 * TODO $propertyLongitude
+	 */
+
+	/*
+	 * TODO $propertyStreetAddress
+	 */
+
+	/*
+	 * TODO $propertyValue
+	 */
 
 
+	/********************************************
+	 * TODO GetFooByBars                        *
+	 ********************************************/
 
+//Closing bracket for Class!!!!!!!!!!!!!!!!!!!
 }
