72d138a785a41a56390539ebdb4d9f6830680698
Lisa Lee <llee28@cnm.edu>
2019-10-29 13:26:12 -0600

    revise attributes and reformat

--- php/public_html/documentation/getfoobybars.php
+++ php/public_html/documentation/getfoobybars.php
@@ -6,19 +6,25 @@
 </head>
 <body>
 <h1>Identifying Needed getFooByBar Methods</h1>
-	<h2>User</h2>
-		<ul>
-			<li>getUserByEmail</li>
-			<li>getUserByUsername</li>
-		</ul>
-	<h2>Property</h2>
-	<ul>
-		<li>getAllProperty</li>
-		<li>getPropertyByUserUuid</li>
-		<li>getPropertyByUserUuidAndBox</li>
-	</ul>
-	<h2>Crime</h2>
-	<ul>
-		<li>getAllCrime</li>
+<h2>User</h2>
+<ul>
+	<li>getUserByEmail</li>
+	<li>getUserByUsername</li>
+</ul>
+<h2>Property</h2>
+<ul>
+	<li>getAllProperty</li>
+	<li>getPropertyByUserUuid</li>
+</ul>
+<h2>Crime</h2>
+<ul>
+	<li>getAllCrime</li>
+</ul>
+<h2>Star</h2>
+<ul>
+	<li>getAllStarPropertyUuid</li>
+	<li>getAllStarUserUuid</li>
+	<li>getStarPropertyByUserUuid</li>
+</ul>
 </body>
 </html>
