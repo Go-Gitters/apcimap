e53f8d339708aece4bdf22ac96b262c49006d554
Lisa Lee <llee28@cnm.edu>
2019-10-29 13:34:29 -0600

    update list of methods for team review

--- php/public_html/documentation/getfoobybars.php
+++ php/public_html/documentation/getfoobybars.php
@@ -8,17 +8,21 @@
 <h1>Identifying Needed getFooByBar Methods</h1>
 <h2>User</h2>
 <ul>
+	<li>getUserByActivationToken</li>
 	<li>getUserByEmail</li>
+	<li>getUserByUserUuid</li>
 	<li>getUserByUsername</li>
 </ul>
 <h2>Property</h2>
 <ul>
 	<li>getAllProperty</li>
+	<li>getPropertyByPropertyUuid</li>
 	<li>getPropertyByUserUuid</li>
 </ul>
 <h2>Crime</h2>
 <ul>
 	<li>getAllCrime</li>
+	<li>getCrimeByCrimeUuid</li>
 </ul>
 <h2>Star</h2>
 <ul>
