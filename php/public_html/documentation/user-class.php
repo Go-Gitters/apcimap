8110cabe1175a61e72ee994cf2c0cb90e302ecb7
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-29 16:22:02 -0600

    updating classes

--- php/public_html/documentation/user-class.php
+++ php/public_html/documentation/user-class.php
@@ -61,23 +61,23 @@ class Profile implements \JsonSerializable {
 		}
 	}
 	/**
-	 * accessor method for profile id
+	 * accessor method for user id
 	 *
 	 * @return Uuid value of profile id (or null if new Profile)
 	 **/
-	public function getProfileId(): Uuid {
+	public function getUserId(): Uuid {
 		return ($this->userUuId);
 	}
 	/**
 	 * mutator method for profile id
 	 *
-	 * @param  Uuid| string $newProfileId value of new profile id
-	 * @throws \RangeException if $newProfileId is not positive
-	 * @throws \TypeError if the profile Id is not
+	 * @param  Uuid| string $newUserId value of new profile id
+	 * @throws \RangeException if $newUserId is not positive
+	 * @throws \TypeError if the user Id is not
 	 **/
-	public function setProfileId($newProfileId): void {
+	public function setUserId($newUserId): void {
 		try {
-			$uuid = self::validateUuid($newProfileId);
+			$uuid = self::validateUuid($newUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
@@ -86,103 +86,48 @@ class Profile implements \JsonSerializable {
 		$this->userUuid = $uuid;
 	}
 	/**
-	 * accessor method for account activation token
+	 * accessor method for user activation token
 	 *
 	 * @return string value of the activation token
 	 */
-	public function getProfileActivationToken() : ?string {
-		return ($this->profileActivationToken);
+	public function getUserActivationToken() : ?string {
+		return ($this->userActivationToken);
 	}
 	/**
 	 * mutator method for account activation token
 	 *
-	 * @param string $newProfileActivationToken
+	 * @param string $newUserActivationToken
 	 * @throws \InvalidArgumentException  if the token is not a string or insecure
 	 * @throws \RangeException if the token is not exactly 32 characters
 	 * @throws \TypeError if the activation token is not a string
 	 */
-	public function setProfileActivationToken(?string $newProfileActivationToken): void {
-		if($newProfileActivationToken === null) {
-			$this->profileActivationToken = null;
+	public function setUserActivationToken(?string $newUserActivationToken): void {
+		if($newUserActivationToken === null) {
+			$this->userActivationToken = null;
 			return;
 		}
-		$newProfileActivationToken = strtolower(trim($newProfileActivationToken));
-		if(ctype_xdigit($newProfileActivationToken) === false) {
+		$newUserActivationToken = strtolower(trim($newUserActivationToken));
+		if(ctype_xdigit($newUserActivationToken) === false) {
 			throw(new\RangeException("user activation is not valid"));
 		}
 		//make sure user activation token is only 32 characters
-		if(strlen($newProfileActivationToken) !== 32) {
+		if(strlen($newUserActivationToken) !== 32) {
 			throw(new\RangeException("user activation token has to be 32"));
 		}
-		$this->profileActivationToken = $newProfileActivationToken;
-	}
-	/**
-	 * accessor method for at handle
-	 *
-	 * @return string value of at handle
-	 **/
-	public function getProfileAtHandle(): string {
-		return ($this->profileAtHandle);
-	}
-	/**
-	 * mutator method for at handle
-	 *
-	 * @param string $newProfileAtHandle new value of at handle
-	 * @throws \InvalidArgumentException if $newAtHandle is not a string or insecure
-	 * @throws \RangeException if $newAtHandle is > 32 characters
-	 * @throws \TypeError if $newAtHandle is not a string
-	 **/
-	public function setProfileAtHandle(string $newProfileAtHandle) : void {
-		// verify the at handle is secure
-		$newProfileAtHandle = trim($newProfileAtHandle);
-		$newProfileAtHandle = filter_var($newProfileAtHandle, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newProfileAtHandle) === true) {
-			throw(new \InvalidArgumentException("profile at handle is empty or insecure"));
-		}
-		// verify the at handle will fit in the database
-		if(strlen($newProfileAtHandle) > 32) {
-			throw(new \RangeException("profile at handle is too large"));
-		}
-		// store the at handle
-		$this->profileAtHandle = $newProfileAtHandle;
-	}
-	/**
-	 * accessor method for profile avatar url
-	 * @return string value of the activation token
-	 */
-	public function getProfileAvatarUrl() : string {
-		return($this->profileAvatarUrl);
-	}
-	/**
-	 * mutator method for at handle
-	 *
-	 * @param string $newProfileAvatarUrl new value of profile avatar URL
-	 * @throws \InvalidArgumentException if $newProfileAvatarUrl is not a string or insecure
-	 * @throws \RangeException if $newProfileAvatarUrl is > 255 characters
-	 * @throws \TypeError if $newAtHandle is not a string
-	 **/
-	public function setProfileAvatarUrl(string $newProfileAvatarUrl) : void {
-		$newProfileAvatarUrl = trim($newProfileAvatarUrl);
-		$newProfileAvatarUrl = filter_var($newProfileAvatarUrl, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		// verify the avatar URL will fit in the database
-		if(strlen($newProfileAvatarUrl) > 255) {
-			throw(new \RangeException("image cloudinary content too large"));
-		}
-		// store the image cloudinary content
-		$this->profileAvatarUrl = $newProfileAvatarUrl;
+		$this->userActivationToken = $newUserActivationToken;
 	}
 	/**
 	 * accessor method for email
 	 *
 	 * @return string value of email
 	 **/
-	public function getProfileEmail(): string {
-		return $this->profileEmail;
+	public function getUserEmail(): string {
+		return $this->userEmail;
 	}
 	/**
 	 * mutator method for email
 	 *
-	 * @param string $newProfileEmail new value of email
+	 * @param string $newUserEmail new value of email
 	 * @throws \InvalidArgumentException if $newEmail is not a valid email or insecure
 	 * @throws \RangeException if $newEmail is > 128 characters
 	 * @throws \TypeError if $newEmail is not a string
@@ -206,15 +151,15 @@ class Profile implements \JsonSerializable {
 	 *
 	 * @return string value of hash
 	 */
-	public function getProfileHash(): string {
-		return $this->profileHash;
+	public function getUserHash(): string {
+		return $this->userHash;
 	}
 	/**
 	 * mutator method for profile hash password
 	 *
-	 * @param string $newProfileHash
+	 * @param string $newUserHash
 	 * @throws \InvalidArgumentException if the hash is not secure
-	 * @throws \RangeException if the hash is not 128 characters
+	 * @throws \RangeException if the hash is not 97 characters
 	 * @throws \TypeError if profile hash is not a string
 	 */
 	public function setProfileHash(string $newProfileHash): void {
