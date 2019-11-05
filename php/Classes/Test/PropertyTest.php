ae296967fd61463bdc37eff93b46505ce5eecc29
Kyla Bendt <kylabendt@gmail.com>
2019-11-05 09:56:05 -0700

    add doc block for class

--- php/Classes/Test/PropertyTest.php
+++ php/Classes/Test/PropertyTest.php
@@ -11,6 +11,18 @@ require_once(dirname(__DIR__) . "/autoload.php");
 // grab the uuid generator
 require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
+/**
+ * Class PropertyTest
+ *
+ * A complete PHPUnit test of the Property class.  It is complete because *ALL* mySQL/PDO enabled methods are tested for
+ * both valid and invalid inputs
+ *
+ * @see Property
+ * @package GoGitters\ApciMap\Test
+ *
+ * @author Kyla Bendt
+ */
+
 
 class PropertyTest {
 
