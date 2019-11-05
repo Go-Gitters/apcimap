48d47051c0477b600ce15a02865e367f90f344de
Kyla Bendt <kylabendt@gmail.com>
2019-11-05 09:53:40 -0700

    add dependencies

--- php/Classes/Test/PropertyTest.php
+++ php/Classes/Test/PropertyTest.php
@@ -3,6 +3,14 @@
 
 namespace GoGitters\ApciMap\Test;
 
+//grab the Property Class
+use GoGitters\ApciMap\{Property};
+
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
 
 class PropertyTest {
 
