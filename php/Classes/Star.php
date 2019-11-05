306717b1dc567501a73ac46f81da1afab186b8a4
Lisa Lee <llee28@cnm.edu>
2019-11-05 15:25:11 -0700

    replace Uuid to Id in variable names

--- php/Classes/Star.php
+++ php/Classes/Star.php
@@ -11,7 +11,7 @@ use Ramsey\Uuid\Uuid;
 /*
  * Star Class
  *
- * This class includes data for starPropertyUuid and starUserUuid.
+ * This class includes data for starPropertyId and starUserId.
  *
  * @author Lisa Lee
  * @version 0.0.1
@@ -22,15 +22,15 @@ class Star {
 
 	/**
 	 * id of the Property being starred; this is a component of a composite primary key (and a foreign key)
-	 * @var Uuid $starPropertyUuid
+	 * @var Uuid $starPropertyId
 	 */
-	private $starPropertyUuid;
+	private $starPropertyId;
 
 	/**
 	 * id of the User who starred; this is a component of a composite primary key (and a foreign key)
-	 * @var Uuid $starUserUuid
+	 * @var Uuid $starUserId
 	 */
-	private $starUserUuid;
+	private $starUserId;
 
 	/********************************************
 	 * Constructor                              *
@@ -38,18 +38,18 @@ class Star {
 	/**
 	 * constructor for this Star
 	 *
-	 * @param string|Uuid $newStarPropertyUuid id of the parent Property
-	 * @param string|Uuid $newStarUserUuid id of the parent User
+	 * @param string|Uuid $newStarPropertyId id of the parent Property
+	 * @param string|Uuid $newStarUserId id of the parent User
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
 	 * @throws \TypeError if a data type violate type hints
 	 * @throws \Exception if some other exception occurs
 	 */
-	public function __construct($newStarPropertyUuid, $newStarUserUuid) {
+	public function __construct($newStarPropertyId, $newStarUserId) {
 		// use the mutator methods to do the work for us
 		try {
-			$this->setStarPropertyUuid($newStarPropertyUuid);
-			$this->setStarUserUuid($newStarUserUuid);
+			$this->setStarPropertyId($newStarPropertyId);
+			$this->setStarUserId($newStarUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 
 			// determine what exception type was thrown
@@ -63,59 +63,59 @@ class Star {
 	 ********************************************/
 
 	/**
-	 * accessor method for property uuid
+	 * accessor method for property id
 	 *
-	 * @return Uuid value of property uuid
+	 * @return Uuid value of property id
 	 */
-	public function getStarPropertyUuid(): Uuid {
-		return ($this->starPropertyUuid);
+	public function getStarPropertyId(): Uuid {
+		return ($this->starPropertyId);
 	}
 
 	/**
-	 * mutator method for Property Uuid
+	 * mutator method for Property id
 	 *
-	 * @param Uuid|string $newStarPropertyUuid new value of property uuid
-	 * @throws \RangeException if $newStarPropertyUuid is not positive
-	 * @throws \TypeError if $newStarPropertyUuid is not a uuid or string
+	 * @param Uuid|string $newStarPropertyId new value of property id
+	 * @throws \RangeException if $newStarPropertyId is not positive
+	 * @throws \TypeError if $newStarPropertyId is not a uuid or string
 	 */
-	public function setStarPropertyUuid($newStarPropertyUuid): void {
+	public function setStarPropertyId($newStarPropertyId): void {
 		try {
-			$newStarPropertyUuid = self::validateUuid($newStarPropertyUuid);
+			$newStarPropertyId = self::validateUuid($newStarPropertyId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 
-		// convert and store the starred property uuid
-		$this->starPropertyUuid = $newStarPropertyUuid;
+		// convert and store the starred property id
+		$this->starPropertyId = $newStarPropertyId;
 	}
 
 	/**
-	 * accessor method for user uuid
+	 * accessor method for user id
 	 *
-	 * @return Uuid value of user uuid
+	 * @return Uuid value of user id
 	 */
-	public function getStarUserUuid(): Uuid {
-		return ($this->starUserUuid);
+	public function getStarUserId(): Uuid {
+		return ($this->starUserId);
 	}
 
 	/**
-	 * mutator method for user Uuid
+	 * mutator method for user id
 	 *
-	 * @param Uuid|string $newStarUserUuid new value of user uuid
-	 * @throws \RangeException if $newStarUserUuid is not positive
-	 * @throws \TypeError if $newStarUserUuid is not a uuid or string
+	 * @param Uuid|string $newStarUserId new value of user id
+	 * @throws \RangeException if $newStarUserId is not positive
+	 * @throws \TypeError if $newStarUserId is not a uuid or string
 	 */
-	public function setStarUserUuid($newStarUserUuid): void {
+	public function setStarUserId($newStarUserId): void {
 		try {
-			$newStarUserUuid = self::validateUuid($newStarUserUuid);
+			$newStarUserId = self::validateUuid($newStarUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 
-		// convert and store the starred user uuid
-		$this->starUserUuid = $newStarUserUuid;
+		// convert and store the starred user id
+		$this->starUserId = $newStarUserId;
 	}
 
 	/**
@@ -127,11 +127,11 @@ class Star {
 	public function insert(\PDO $pdo): void {
 
 		// create query template
-		$query = "INSERT INTO star(starPropertyUuid, starUserUuid) VALUES(:starPropertyUuid,:starUserUuid)";
+		$query = "INSERT INTO star(starPropertyId, starUserId) VALUES(:starPropertyId,:starUserId)";
 		$statement = $pdo->prepare($query);
 
 		// bind the member variables to the placeholders in the template
-		$parameters = ["starPropertyUuid" => $this->starPropertyUuid->getBytes(), "starUserUuid" => $this->starUserUuid->getBytes()];
+		$parameters = ["starPropertyId" => $this->starPropertyId->getBytes(), "starUserId" => $this->starUserId->getBytes()];
 		$statement->execute($parameters);
 	}
 
@@ -145,40 +145,40 @@ class Star {
 
 		// create query template
 		// NOTE: which query do we need? do we need both?
-		$query = "DELETE FROM star WHERE starPropertyUuid = :starPropertyUuid AND starUserUuid = :starUserUuid";
+		$query = "DELETE FROM star WHERE starPropertyId = :starPropertyId AND starUserId = :starUserId";
 		$statement = $pdo->prepare($query);
 
 		// bind the member variables to the placeholder in the template
-		$parameters = ["starPropertyUuid" => $this->starPropertyUuid->getBytes(), "starUserUuid" => $this->starUserUuid->getBytes()];
+		$parameters = ["starPropertyId" => $this->starPropertyId->getBytes(), "starUserId" => $this->starUserId->getBytes()];
 		$statement->execute($parameters);
 	}
 
 	/**********************************************
-	 *    GetFooByBars - getStarByPropertyUuid    *
+	 *    GetFooByBars - getStarByPropertyId    *
 	 **********************************************/
 
 	/**
-	 * gets the Star by Property Uuid
+	 * gets the Star by Property Id
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param Uuid|string $starPropertyUuid property uuid to search for
+	 * @param Uuid|string $starPropertyId property id to search for
 	 * @return \SplFixedArray SplFixedArray of Stars found or null if not found
 	 * @throws \PDOException when mySQL related errors occur
 	 */
-	public static function getStarByStarPropertyUuid(\PDO $pdo, $starPropertyUuid): \SplFixedArray {
-		// sanitize the property uuid before searching
+	public static function getStarByStarPropertyId(\PDO $pdo, $starPropertyId): \SplFixedArray {
+		// sanitize the property id before searching
 		try {
-			$starPropertyUuid = self::validateUuid($starPropertyUuid);
+			$starPropertyId = self::validateUuid($starPropertyId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
 		// create query template
-		$query = "SELECT starPropertyUuid, starUserUuid FROM star WHERE starPropertyUuid = :starPropertyUuid";
+		$query = "SELECT starPropertyId, starUserId FROM star WHERE starPropertyId = :starPropertyId";
 		$statement = $pdo->prepare($query);
 
-		// bind the star property uuid to the placeholder in the template
-		$parameters = ["starPropertyUuid" => $starPropertyUuid->getBytes()];
+		// bind the star property id to the placeholder in the template
+		$parameters = ["starPropertyId" => $starPropertyId->getBytes()];
 		$statement->execute($parameters);
 
 		// build an array of stars
@@ -186,7 +186,7 @@ class Star {
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while(($row = $statement->fetch()) !== false) {
 			try {
-				$star = new Star($row["starPropertyUuid"], $row["starUserUuid"]);
+				$star = new Star($row["starPropertyId"], $row["starUserId"]);
 				$stars[$stars->key()] = $star;
 				$stars->next();
 			} catch(\Exception $exception) {
@@ -198,31 +198,31 @@ class Star {
 
 	}
 	/********************************************
-	 *     GetFooByBars - getStarByUserUuid     *
+	 *     GetFooByBars - getStarByUserId     *
 	 ********************************************/
 
 	/**
-	 * gets the Star by user Uuid
+	 * gets the Star by user id
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param Uuid|string $starUserUuid star user uuid to search for
+	 * @param Uuid|string $starUserId star user id to search for
 	 * @return \SplFixedArray array of Stars found or null if not found
 	 * @throws \PDOException when mySQL related errors occur
 	 * \    */
-	public static function getStarByUserUuid(\PDO $pdo, $starUserUuid): \SplFixedArray {
-		// sanitize the user uuid before searching
+	public static function getStarByUserId(\PDO $pdo, $starUserId): \SplFixedArray {
+		// sanitize the user id before searching
 		try {
-			$starUserUuid = self::validateUuid($starUserUuid);
+			$starUserId = self::validateUuid($starUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
 		// create query template
-		$query = "SELECT starPropertyUuid, starUserUuid FROM star WHERE starUserUuid = :starUserUuid";
+		$query = "SELECT starPropertyId, starUserId FROM star WHERE starUserId = :starUserId";
 		$statement = $pdo->prepare($query);
 
-		// bind the star user uuid to the placeholder in the template
-		$parameters = ["starUserUuid" => $starUserUuid->getBytes()];
+		// bind the star user id to the placeholder in the template
+		$parameters = ["starUserId" => $starUserId->getBytes()];
 		$statement->execute($parameters);
 
 		// build an array of stars
@@ -230,7 +230,7 @@ class Star {
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while(($row = $statement->fetch()) !== false) {
 			try {
-				$star = new Star($row["starPropertyUuid"], $row["starUserUuid"]);
+				$star = new Star($row["starPropertyId"], $row["starUserId"]);
 				$stars[$stars->key()] = $star;
 				$stars->next();
 			} catch(\Exception $exception) {
@@ -242,37 +242,37 @@ class Star {
 	}
 
 	/***********************************************************
-	 *     GetFooByBars - getStarByPropertyUuidAndUserUuid    *
+	 *     GetFooByBars - getStarByPropertyIdAndUserId    *
 	 ***********************************************************/
 
 	/**
-	 * gets the Star by property Uuid and user Uuid
+	 * gets the Star by property id and user id
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param Uuid|string $starPropertyUuid property uuid to search for
-	 * @param Uuid|string $starUserUuid user uuid to search for
+	 * @param Uuid|string $starPropertyId property id to search for
+	 * @param Uuid|string $starUserId user id to search for
 	 * @return Star|null Star found or null if not found
 	 */
-	public static function getStarByStarPropertyUuidAndStarUserUuid(\PDO $pdo, $starPropertyUuid, $starUserUuid): ?Star {
+	public static function getStarByStarPropertyIdAndStarUserId(\PDO $pdo, $starPropertyId, $starUserId): ?Star {
 
 		try {
-			$starPropertyUuid = self::validateUuid($starPropertyUuid);
+			$starPropertyId = self::validateUuid($starPropertyId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
 		try {
-			$starUserUuid = self::validateUuid($starUserUuid);
+			$starUserId = self::validateUuid($starUserId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
 		// create query template
-		$query = "SELECT starPropertyUuid, starUserUuid FROM star WHERE starPropertyUuid = :starPropertyUuid AND starUserUuid = :starUserUuid";
+		$query = "SELECT starPropertyId, starUserId FROM star WHERE starPropertyId = :starPropertyId AND starUserId = :starUserId";
 		$statement = $pdo->prepare($query);
 
-		// bind the property uuid and user uuid to the placeholder in the template
-		$parameters = ["starPropertyUuid" => $starPropertyUuid->getBytes(), "starUserUuid" => $starUserUuid->getBytes()];
+		// bind the property id and user id to the placeholder in the template
+		$parameters = ["starPropertyId" => $starPropertyId->getBytes(), "starUserId" => $starUserId->getBytes()];
 		$statement->execute($parameters);
 
 		// grab the star from mySQL
@@ -281,7 +281,7 @@ class Star {
 			$statement->setFetchMode(\PDO::FETCH_ASSOC);
 			$row = $statement->fetch();
 			if($row !== false) {
-				$star = new Star($row["starPropertyUuid"], $row["starUserUuid"]);
+				$star = new Star($row["starPropertyId"], $row["starUserId"]);
 			}
 		} catch(\Exception $exception) {
 			// if the row couldn't be converted, rethrow it
