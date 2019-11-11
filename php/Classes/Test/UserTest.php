88571a675414a7e2b46c563cb7c294af626431db
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-11 15:34:18 -0700

    adding a get user by email function

--- php/Classes/Test/UserTest.php
+++ php/Classes/Test/UserTest.php
@@ -28,6 +28,47 @@ class UserTest extends ApciMapTest {
 
 
 
+
+	
+	/**
+	 * test grabbing a User by email
+	 **/
+	public function testGetValidUserByEmail() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("user");
+		$userId = generateUuidV4();
+		$user = new User($userId,$this->VALID_ACTIVATION $this->VALID_EMAIL, $this->VALID_HASH, $this->VALID_USERNAME)
+		$profile->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$userProfile = User::getUserByUserEmail($this->getPDO(), $user->getUserEmail());
+		$this->assertEquals($pdoUser->getUserId(), $userId);
+		$this->assertEquals($pdoUser->getUserActivationToken(), $this->VALID_ACTIVATION);
+		$this->assertEquals($pdoUser->getUserEmail(), $this->VALID_USEREMAIL);
+		$this->assertEquals($pdoUser->getUserHash(), $this->VALID_HASH);
+		$this->assertEquals($pdoUser->get(), $this->VALID_EMAIL);
+		$this->assertEquals($pdoUser->getUserUsername(), $this->VALID_USERNAME);
+	}
+	/**
+	 * test grabbing a Profile by an email that does not exists
+	 **/
+	public function testGetInvalidProfileByEmail() : void {
+		// grab an email that does not exist
+		$profile = Profile::getProfileByProfileEmail($this->getPDO(), "does@not.exist");
+		$this->assertNull($profile);
+	}
+
+
+
+
+	/**
+	 * test grabbing a profile by its activation
+	 */
+	public function testGetValidUserByActivationToken() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("user");
+		$userId = generateUuidV4();
+		$user = new User($userId,$this->VALID_ACTIVATION $this->VALID_EMAIL, $this->VALID_HASH, $this->VALID_USERNAME)
+		$user->insert($this->getPDO());
 	// grab the data from mySQL and enforce the fields match our expectations
 $pdoUser = User::getUserByUserActivationToken($this->getPDO(), $user->getUserActivationToken());
 $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
@@ -36,8 +77,7 @@ $this->assertEquals($pdoUser->getUserActivationToken(), $this->VALID_ACTIVATION)
 $this->assertEquals($pdoUser->getUserEmail(), $this->VALID_USEREMAIL);
 $this->assertEquals($pdoUser->getUserHash(), $this->VALID_HASH);
 $this->assertEquals($pdoUser->get(), $this->VALID_EMAIL);
-$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_HASH);
-$this->assertEquals($pdoProfile->getProfilePhone(), $this->VALID_PHONE);
+$this->assertEquals($pdoUser->getUserUsername(), $this->VALID_USERNAME);
 }
 	/**
 	 * test grabbing a Profile by an email that does not exists
