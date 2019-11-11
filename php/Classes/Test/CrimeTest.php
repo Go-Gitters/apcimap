011da6abb1cdee69c2812f8e55979f209a93f677
Lisa Lee <llee28@cnm.edu>
2019-11-11 15:51:54 -0700

    add create new Crime and insert into mySQL under test to grab all Crimes

--- php/Classes/Test/CrimeTest.php
+++ php/Classes/Test/CrimeTest.php
@@ -244,6 +244,13 @@ class CrimeTest extends ApciMapTest {
 		public function testGetAllValidCrimes() : void {
 			// count the number of rows and save it for later
 			$numRows = $this->getConnection()->getRowCount("crime");
+
+			// create a new Crime and insert into mySQL
+			$crimeId = generateUuidV4();
+			$crime = new Crime($crimeId, $this->VALID_CRIMEADDRESS, $this->VALID_CRIMEDATE, $this->VALID_CRIMELATITUDE, $this->VALID_CRIMELONGITUDE, $this->VALID_CRIMETYPE);
+			$crime->insert($this->getPDO());
+
+
 		}
 	}
 }
