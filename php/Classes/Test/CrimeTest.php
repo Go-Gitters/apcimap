0be8d78a03fc991f29bb8d9c45d36ffc7f5ccde2
Lisa Lee <llee28@cnm.edu>
2019-11-11 16:08:29 -0700

    correct incorrect parsing on lines 233 & 242

--- php/Classes/Test/CrimeTest.php
+++ php/Classes/Test/CrimeTest.php
@@ -230,8 +230,7 @@ class CrimeTest extends ApciMapTest {
 		/**
 		 * test grabbing a Crime by a crime incident report type that does not exist
 		 **/
-		public
-		function testGetInvalidCrimeByCrimeType(): void {
+		public function testGetInvalidCrimeByCrimeType(): void {
 			// grab a crime by crime incident report type that does not exist
 			$crime = Crime::getCrimeByCrimeType($this->getPDO(), "kejalek jfalek fjeio aejl");
 			$this->assertCount(0, $crime);
@@ -240,8 +239,7 @@ class CrimeTest extends ApciMapTest {
 		/**
 		 * test grabbing all Crimes
 		 **/
-		public
-		function testGetAllValidCrimes(): void {
+		public function testGetAllValidCrimes(): void {
 			// count the number of rows and save it for later
 			$numRows = $this->getConnection()->getRowCount("crime");
 
