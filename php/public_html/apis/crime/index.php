b7cabcefea1b939b8c4076c443210beb69679c55
Lisa Lee <llee28@cnm.edu>
2019-11-18 13:30:05 -0700

    create crime directory for api

---
+++ php/public_html/apis/crime/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
+use GoGitters\ApciMap\{User, Property, Crime, Star};
+
+/**
+ *
+ **/
