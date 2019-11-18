b40a1ed96423652aad25c3c1b0f9affefdcdc2a4
Kyla Bendt <kylabendt@gmail.com>
2019-11-18 13:22:35 -0700

    filter input

--- php/public_html/apis/property/index.php
+++ php/public_html/apis/property/index.php
@@ -12,10 +12,11 @@ use GoGitters\ApciMap\{Property};
 
 /**
  * api for the Property Class
+ *
+ *
  * @author Kyla Bendt <kylabendt@gmail.com>
  *
  **/
-
 
 //verify the session, start if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
@@ -25,3 +26,28 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
+try {
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/map.ini");
+	$pdo = $secrets->getPdoObject();
+	//determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+	//sanitize input
+	//TODO: Delete this line?
+	//$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$lat = filter_input(INPUT_GET, "lat", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$long = filter_input(INPUT_GET, "long", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$distance = filter_input(INPUT_GET, "distance", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	//make sure the id is valid for methods that require it
+	//TODO: Delete this too?
+//	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true )) {
+//		throw(new InvalidArgumentException("id cannot be empty or negative", 402));
+//	}
+
+
+
+
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
