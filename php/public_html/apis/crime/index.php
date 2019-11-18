3665dfdcbcffaa4204171b78a0db5c0cbd4d9b1b
Lisa Lee <llee28@cnm.edu>
2019-11-18 13:33:45 -0700

    add require_once, verify session, and prep empty reply

--- php/public_html/apis/crime/index.php
+++ php/public_html/apis/crime/index.php
@@ -11,5 +11,19 @@ require_once("/etc/apache2/capstone-mysql/Secrets.php");
 use GoGitters\ApciMap\{User, Property, Crime, Star};
 
 /**
+ * api for Crime class
  *
+ * @author Lisa Lee
  **/
+// verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+
+	$secrets =
