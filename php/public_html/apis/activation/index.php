1527d82bc80b4e542fe7da56ed4a42e135123f0e
Lisa Lee <llee28@cnm.edu>
2019-11-18 11:39:06 -0700

    add check session, prepare empty reply, grab MySQL connection:

--- php/public_html/apis/activation/index.php
+++ php/public_html/apis/activation/index.php
@@ -8,3 +8,22 @@ use GoGitters\ApciMap\User;
  * API to check user activation status
  * @author Lisa Lee
  **/
+// check the session; if it is not active, start the session
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try{
+	// grab the MySQL connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/map.ini");
+	$pdo = $secrets->getPdoObject();
+
+	// check the HTTP method being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+
+}
