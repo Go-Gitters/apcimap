55fbc0c5c5b4011e352bd196ee3e926720391b87
Lisa Lee <llee28@cnm.edu>
2019-11-26 14:44:01 -0700

    clean up code

--- php/public_html/apis/star/index.php
+++ php/public_html/apis/star/index.php
@@ -8,6 +8,7 @@ require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 
 use GoGitters\ApciMap\Star;
+
 /**
  * API for the Star class
  *
@@ -33,32 +34,29 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize the search parameters
-	$starUserId = filter_input(INPUT_GET, "starUserId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-	$starPropertyId = filter_input(INPUT_GET, "starPropertyId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$starUserId = filter_input(INPUT_GET, "starUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$starPropertyId = filter_input(INPUT_GET, "starPropertyId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
 
 		//gets a specific star associated based on its composite key
-		if (($starPropertyId !== null) && ($starUserId !== null)) {
+		if(($starPropertyId !== null) && ($starUserId !== null)) {
 			$star = Star::getStarByStarPropertyIdAndStarUserId($pdo, $starPropertyId, $starUserId);
 
-			if($star!== null) {
+			if($star !== null) {
 				$reply->data = $star;
 			}
-			//if none of the search parameters are met throw an exception
+			//if star user id is present, get stars by user id
 		} else if(empty($starUserId) === false) {
 			$reply->data = Star::getStarByUserId($pdo, $starUserId)->toArray();
 
-			//get all the stars associated with the property Id
-		} else if(empty($starPropertyId) === false) {
-			$reply->data = Star::getStarByStarPropertyId($pdo, $starPropertyId)->toArray();
 		} else {
 			throw new InvalidArgumentException("incorrect search parameters ", 404);
 		}
 
-	} else if($method === "POST" || $method === "PUT") {
+	} else if($method === "POST") {
 
 		//decode the response from the front end
 		$requestContent = file_get_contents("php://input");
@@ -71,61 +69,51 @@ try {
 		if(empty($requestObject->starPropertyId) === true) {
 			throw (new \InvalidArgumentException("No property linked to the star", 405));
 		}
-// remove date request - since date not used in star class
-//		if(empty($requestObject->starDate) === true) {
-//			$requestObject->StarDate =  date("y-m-d H:i:s.u");
-//		}
 
-		if($method === "POST") {
-			//enforce that the end user has a XSRF token.
-			verifyXsrf();
+		//enforce that the end user has a XSRF token.
+		verifyXsrf();
 
-			//enforce the end user has a JWT token
-			//validateJwtHeader();
+		//enforce the end user has a JWT token
+		validateJwtHeader();
 
-			// enforce the user is signed in
-			if(empty($_SESSION["user"]) === true) {
-				throw(new \InvalidArgumentException("you must be logged in to star posts", 403));
-			}
-
-			validateJwtHeader();
-
-			$star = new Star($requestObject->starPropertyId, $_SESSION["user"]->getUserId());
-			$star->insert($pdo);
-			$reply->message = "starred property successful";
-
-		} else if($method === "PUT") {
-
-			//enforce the end user has a XSRF token.
-			verifyXsrf();
-
-			//enforce the end user has a JWT token
-			validateJwtHeader();
-
-			//grab the star by its composite key
-			$star = Star::getStarByStarPropertyIdAndStarUserId($pdo, $requestObject->starPropertyId, $requestObject->starUserId);
-			if($star === null) {
-				throw (new RuntimeException("Star does not exist"));
-			}
-
-			//enforce the user is signed in and only trying to edit their own star
-			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $star->getStarUserId()->toString()) {
-				throw(new \InvalidArgumentException("You are not allowed to delete this property", 403));
-			}
-
-			//validateJwtHeader();
-
-			//preform the actual delete
-			$star->delete($pdo);
-
-			//update the message
-			$reply->message = "Star successfully deleted";
+		// enforce the user is signed in
+		if(empty($_SESSION["user"]) === true) {
+			throw(new \InvalidArgumentException("you must be logged in to star posts", 403));
 		}
 
-		// if any other HTTP request is sent throw an exception
-	} else {
-		throw new \InvalidArgumentException("invalid http request", 400);
+		$star = new Star($requestObject->starPropertyId, $_SESSION["user"]->getUserId());
+		$star->insert($pdo);
+		$reply->message = "starred property successful";
+
+	} else if($method === "DELETE") {
+
+		//enforce the end user has a XSRF token.
+		verifyXsrf();
+
+		//enforce the end user has a JWT token
+		validateJwtHeader();
+
+		//grab the star by its composite key
+		$star = Star::getStarByStarPropertyIdAndStarUserId($pdo, $starPropertyId, $_SESSION["user"]->getUserId());
+		if($star === null) {
+			throw (new RuntimeException("Star does not exist"));
+		}
+
+		//enforce the user is signed in and only trying to edit their own star
+		if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $star->getStarUserId()->toString()) {
+			throw(new \InvalidArgumentException("You are not allowed to delete this property", 403));
+		}
+
+		//preform the actual delete
+		$star->delete($pdo);
+
+		//update the message
+		$reply->message = "Star successfully deleted";
 	}
+	// if any other HTTP request is sent throw an exception
+	 else {
+	throw new \InvalidArgumentException("invalid http request", 400);
+}
 	//catch any exceptions that is thrown and update the reply status and message
 } catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
