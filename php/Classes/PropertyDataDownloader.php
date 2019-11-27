4778fa68b7497a571369dfea51d30909b1e4f49d
Kyla Bendt <kylabendt@gmail.com>
2019-11-27 11:41:27 -0700

    make property data downloader file

---
+++ php/Classes/PropertyDataDownloader.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace GoGitters\ApciMap;
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 1) . "/lib/uuid.php");
+
+
+/**
+ * This class takes the property data as a JSON format and sticks it in the database
+**/
+class PropertyDataDownloader {
+
+}
