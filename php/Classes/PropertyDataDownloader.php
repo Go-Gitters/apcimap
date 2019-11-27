bdbfc90c319b5572504089105c04c81b17c6589e
Kyla Bendt <kylabendt@gmail.com>
2019-11-27 11:53:20 -0700

    start writing data downloader

--- php/Classes/PropertyDataDownloader.php
+++ php/Classes/PropertyDataDownloader.php
@@ -11,4 +11,23 @@ require_once(dirname(__DIR__, 1) . "/lib/uuid.php");
 **/
 class PropertyDataDownloader {
 
+	public static function pullProperties() {
+		$newProperties = null;
+
+		$urlBase = "https://bootcamp-coders.cnm.edu/~kbendt/apcimap/data/prop.json";
+		$secrets = new \Secrets("/etc/apache2/capstone-mysql/map.ini");
+		$pdo = $secrets->getPdoObject();
+
+		$newProperties = self::readDataJson($urlBase);
+
+		foreach($newProperties as $value) {
+			$propertyId = generateUuidV4();
+			$property
+
+
+		}
+
+	}
+
+
 }
