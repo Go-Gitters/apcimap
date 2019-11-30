d7f593ee31104dfb68e18d52cf2c03987f8d8579
Lisa Lee <llee28@cnm.edu>
2019-11-30 11:53:50 -0700

    add function that returns a count of rows in crime incident reports table

--- php/Classes/CrimeDataDownloader.php
+++ php/Classes/CrimeDataDownloader.php
@@ -9,7 +9,7 @@ require_once("./Crime.php");
 
 /**
  * This class takes the crime incident reports data as a JSON format and sticks it in the database
- */
+ **/
 class CrimeDataDownloader {
 
 	/**
@@ -17,7 +17,7 @@ class CrimeDataDownloader {
 	 *
 	 * @param $urlBase string url to get json data from
 	 * @throws \Exception
-	 */
+	 **/
 	public static function pullCrimes($urlBase) {
 		$newCrimes = null;
 
@@ -44,5 +44,36 @@ class CrimeDataDownloader {
 		echo("Done");
 	}
 
-	public static function read
+	public static function readDataJson($url) {
+		$context = stream_context_create(["http" => ["ignore_errors" => true, "method" => "GET"]]);
+		// file-get-contents returns file in string context
+		if(($jsonData = file_get_contents($url, null, $context)) === false) {
+			throw(new \RuntimeException("url doesn't produce results"));
+		}
+		// decode the Json file
+		$jsonConverted = json_decode($jsonData);
+
+		// pull out the crime incident reports array
+		$jsonFeatures = $jsonConverted->crimes;
+		$newCrimes = \SplFixedArray::fromArray($jsonFeatures);
+		return ($newCrimes);
+	}
+
+	/**
+	 * This function returns a count of rows in the crime incident reports table
+	 **/
+	public static function crimeCount() {
+		$secrets = new \Secrets("/etc/apache2/capstone-mysql/map.ini");
+		$pdo = $secrets->getPdoObject();
+		$query = "SELECT COUNT(*) FROM crime";
+		$statement = $pdo->prepare($query);
+		$statement->execute();
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		$row = $statement->fetch();
+		echo("crime incident reports count = " . $row['COUNT(*)']);
+	}
 }
+// url to get json data from
+$url1 = "https://bootcamp-coders.cnm.edu/~llee28/apcimap/data/crime.json";
+
+echo CrimeDataDownloader::crimeCount();
