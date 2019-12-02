6a53dc804841633a2afb699fcad36a95e3a9bd5c
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-12-02 16:27:36 -0700

    adding initial navbar content

---
+++ app/src/pages/Header/header.js
@@ -0,0 +1,9 @@
+<Navbar bg="light" expand="lg">
+	<Navbar.Brand href="#home">APCIMAP</Navbar.Brand>
+	<Navbar.Toggle aria-controls="basic-navbar-nav" />
+	<Navbar.Collapse id="basic-navbar-nav">
+	<Nav className="mr-auto">
+	<Nav.Link href="#home">Map</Nav.Link>
+	<Nav.Link href="#link">Sign Up</Nav.Link>
+		<Nav.Link href="#link">Log In</Nav.Link>
+</Nav>
