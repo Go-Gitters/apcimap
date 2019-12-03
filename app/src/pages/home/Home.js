60dc228d6478e37dab475553bb20864582afcc36
Kyla Bendt <kylabendt@gmail.com>
2019-12-03 14:35:19 -0700

    A mapgit add package-lock.json src/pages/home/Home.js !  I made a mapgit add package-lock.json src/pages/home/Home.js ... And it movesgit add package-lock.json src/pages/home/Home.js

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
 
 
-
 export const Home = () => {
-	const state = {
-		viewport: {
-			width: 400,
-			height: 400,
-			latitude: 37.7577,
-			longitude: -122.4376,
-			zoom: 8
-		}
-	}
+	const [mapboxViewport, setMapboxViewport] = useState({
+		width: 800,
+		height: 600,
+		latitude: 35,
+		longitude: -107,
+		zoom: 8
+	});
 	return (
 		<>
-			<h1>Home</h1>
 
 			<ReactMapGL
-				{...state.viewport}
 				mapboxApiAccessToken='pk.eyJ1Ijoia3lsYWJlbmR0IiwiYSI6ImNrM25oZW5hMjFnYm4zbG40d3ljNWRwMTUifQ.vZe28vwUNPkzafr1vuHDtQ'
+				 mapStyle={'mapbox://styles/kylabendt/ck3ni0wwo4fxi1cpv0qchtsml'}
+				{...mapboxViewport}
+				onViewportChange={(viewport) => {
+					setMapboxViewport((viewport))
+				}}
 
 			/>
 		</>
-	)
+	);
 }
