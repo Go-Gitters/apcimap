0673c325d65c17262f0069e5a30710f03d733611
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-12-03 13:02:57 -0700

    adding base content for forms

--- app/src/pages/Header/header.js
+++ app/src/pages/Header/header.js
@@ -1,3 +1,10 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {LinkContainer} from "react-router-bootstrap"
+import {SignUpModal} from "./sign-up/SignUpModal";
+import {SignInModal} from "./sign-in/SigninModal";
+
 <Navbar bg="light" expand="lg">
 	<Navbar.Brand href="#home">APCIMAP</Navbar.Brand>
 	<Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -6,4 +13,5 @@
 	<Nav.Link href="#home">Map</Nav.Link>
 	<Nav.Link href="#link">Sign Up</Nav.Link>
 		<Nav.Link href="#link">Log In</Nav.Link>
+		<Nav.Link href="#link">About Us</Nav.Link>
 </Nav>
---
+++ app/src/pages/Login/loginFormContent.js
@@ -0,0 +1,29 @@
+import {FormDebugger} from "../../FormDebugger";
+import React from "react";
+
+export const SignInFormContent = (props) => {
+	const {
+		status,
+		values,
+		errors,
+		touched,
+		dirty,
+		isSubmitting,
+		handleChange,
+		handleBlur,
+		handleSubmit,
+		handleReset
+	} = props;
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				{/*controlId must match what is passed to the initialValues prop*/}
+				<div className="form-group">
+					<label htmlFor="userEmail">Email Address</label>
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<div className="input-group-text">
+								<FontAwesomeIcon icon="envelope"/>
+							</div>
+						</div>
+		
