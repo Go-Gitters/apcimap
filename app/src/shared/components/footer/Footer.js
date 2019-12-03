1118f0818878b2f0c688929e15098adf13037dad
Lisa Lee <llee28@cnm.edu>
2019-12-03 14:00:10 -0700

    add footer text

--- app/src/shared/components/footer/Footer.js
+++ app/src/shared/components/footer/Footer.js
@@ -13,16 +13,25 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Footer = () => (
 	<>
-	<footer className="page-footer text-muted py-2 py-md-4">
-	<Container fluid="true">
-	<Row>
-	<Col className="text-center">
-	<FontAwesomeIcon icon={['fab', 'github']} /> &nbsp;
+		<footer className="page-footer text-muted py-2 py-md-4">
+			<Row>
+				<Col>About Us</Col>
+				<Col>Source Code on GitHub</Col>
+				<Col>License</Col>
+			</Row>
+			<Row>
+				<Col>by Go-Gitters – a CNM Deep Dive Bootcampp group</Col>
+			</Row>
+			{/*<Container fluid="true">*/}
+			{/*	<Row>*/}
+			{/*		<Col className="text-center">*/}
+			{/*			<FontAwesomeIcon icon={['fab', 'github']}/> &nbsp;*/}
 
-<a href="https://github.com/Go-Gitters/apcimap" className="text-muted" target="_blank" rel="noopener noreferrer">GitHub</a>
-	</Col>
-	</Row>
-	</Container>
-	</footer>
+			{/*			<a href="https://github.com/Go-Gitters/apcimap" className="text-muted" target="_blank"*/}
+			{/*				rel="noopener noreferrer">Source Code on GitHub</a>*/}
+			{/*		</Col>*/}
+			{/*	</Row>*/}
+			{/*</Container>*/}
+		</footer>
 	</>
 );
