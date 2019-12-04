dbaa572fb21d78a99fdafa52e51d466d314af8ee
Lisa Lee <llee28@cnm.edu>
2019-12-04 08:14:44 -0700

    comment out ModalFooter

--- app/src/index.js
+++ app/src/index.js
@@ -7,6 +7,7 @@ import {FourOhFour} from "./pages/four-oh-four/FourOhFour";
 import {Home} from "./pages/home/Home";
 
 
+// import ModalFooter from 'react-bootstrap/ModalFooter';
 
 import {Footer} from "./shared/components/footer/Footer";
 
