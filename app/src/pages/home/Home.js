3700644a6a703c0ebac026a4f393c2bdcd9ea261
Kyla Bendt <kylabendt@gmail.com>
2019-12-04 11:04:16 -0700

    center map & add padding

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,28 +1,30 @@
 import React, {useState} from "react";
-import {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
 
 
 export const Home = () => {
 	const [mapboxViewport, setMapboxViewport] = useState({
 		width: 800,
-		height: 600,
-		latitude: 35,
-		longitude: -107,
-		zoom: 8
+		height: 700,
+		latitude: 35.1129685,
+		longitude: -106.5670637,
+		zoom: 12
 	});
 	return (
-		<>
-
-			<ReactMapGL
-				mapboxApiAccessToken='pk.eyJ1Ijoia3lsYWJlbmR0IiwiYSI6ImNrM25oZW5hMjFnYm4zbG40d3ljNWRwMTUifQ.vZe28vwUNPkzafr1vuHDtQ'
-				 mapStyle={'mapbox://styles/kylabendt/ck3ni0wwo4fxi1cpv0qchtsml'}
-				{...mapboxViewport}
-				onViewportChange={(viewport) => {
-					setMapboxViewport((viewport))
-				}}
-
-			/>
-		</>
+		<div className="">
+				<div className="d-flex justify-content-center p-5">
+					<>
+						<ReactMapGL
+							className="border border-dark d-inline-block"
+							mapboxApiAccessToken='pk.eyJ1Ijoia3lsYWJlbmR0IiwiYSI6ImNrM25oZW5hMjFnYm4zbG40d3ljNWRwMTUifQ.vZe28vwUNPkzafr1vuHDtQ'
+							mapStyle={'mapbox://styles/kylabendt/ck3ni0wwo4fxi1cpv0qchtsml'}
+							{...mapboxViewport}
+							onViewportChange={(viewport) => {
+								setMapboxViewport((viewport))
+							}}
+						/>
+					</>
+				</div>
+		</div>
 	);
 }
