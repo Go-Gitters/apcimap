2181ef8c4a891c44d39d1d54703e4ba6e5118e67
Kyla Bendt <kylabendt@gmail.com>
2019-12-04 11:20:07 -0700

    bream map out as a seperate component

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,30 +1,14 @@
 import React, {useState} from "react";
-import ReactMapGL from 'react-map-gl';
+import {Map} from "../../shared/components/map/map";
 
 
 export const Home = () => {
-	const [mapboxViewport, setMapboxViewport] = useState({
-		width: 800,
-		height: 700,
-		latitude: 35.1129685,
-		longitude: -106.5670637,
-		zoom: 12
-	});
+
 	return (
-		<div className="">
+
 				<div className="d-flex justify-content-center p-5">
-					<>
-						<ReactMapGL
-							className="border border-dark d-inline-block"
-							mapboxApiAccessToken='pk.eyJ1Ijoia3lsYWJlbmR0IiwiYSI6ImNrM25oZW5hMjFnYm4zbG40d3ljNWRwMTUifQ.vZe28vwUNPkzafr1vuHDtQ'
-							mapStyle={'mapbox://styles/kylabendt/ck3ni0wwo4fxi1cpv0qchtsml'}
-							{...mapboxViewport}
-							onViewportChange={(viewport) => {
-								setMapboxViewport((viewport))
-							}}
-						/>
-					</>
+					<Map />
 				</div>
-		</div>
+
 	);
 }
