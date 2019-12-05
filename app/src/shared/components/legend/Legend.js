08ff30a9439b1df169bdbcea3a530233ecc529a2
Lisa Lee <llee28@cnm.edu>
2019-12-05 15:42:45 -0700

    create legend directory

---
+++ app/src/shared/components/legend/Legend.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+import "../../style.css";
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+
+export const Legend = () => {
+	return (
+		<>
+			<main className="h-80 mt-5 py-5 d-flex align-items-center">
+				<Container fluid="true">
+					<Row>
+						<Col md>
+						</Col>
+					</Row>
+				</Container>
+			</main>
+		</>
+	)
+};
