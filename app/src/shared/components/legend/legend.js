605513bc506a3ee319dfa8b7ef4c0a5be3cf5504
Lisa Lee <llee28@cnm.edu>
2019-12-06 09:58:32 -0700

    adjust col for values

--- app/src/shared/components/legend/legend.js
+++ app/src/shared/components/legend/legend.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-import "../../style.css";
+import "../../../style.css";
 
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
@@ -11,14 +11,119 @@ import Card from "react-bootstrap/Card";
 export const Legend = () => {
 	return (
 		<>
-			<main className="h-80 mt-5 py-5 d-flex align-items-center">
-				<Container fluid="true">
-					<Row>
-						<Col md>
-						</Col>
-					</Row>
-				</Container>
-			</main>
+			<div className="border border-dark px-2">
+			<div className="container py-2">
+				<div className="row p-2" id="legend-name">
+					Assessed Property Value *
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-1">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-2">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-3">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-4">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-5">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-6">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-7">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-8">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-9">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-4" id="color-box-bracket-10">
+					</div>
+					<div className="col">
+						$xx,xxx.xx - $xxx,xxx.xx
+					</div>
+				</div>
+			</div>
+
+			<div className="container py-2">
+				<div className="row" id="footnote">
+					* Range of assessed property values broken into deciles
+				</div>
+			</div>
+			</div>
 		</>
 	)
 };
