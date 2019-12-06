e09f7ca1d78d37fb52e1016d5624f80fe2d97fa8
Lisa Lee <llee28@cnm.edu>
2019-12-06 09:57:34 -0700

    adjust padding and make rows stack for mobile

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,14 +1,28 @@
 import React, {useState} from "react";
 import {Map} from "../../shared/components/map/map";
+import {Legend} from "../../shared/components/legend/legend";
 
+//REACT BOOTSTRAP CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Home = () => {
 
 	return (
-
-				<div className="d-flex justify-content-center p-5">
-					<Map />
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-8">
+			<div className="d-flex justify-content-center p-3">
+				<Map/>
+			</div>
+					</div>
+					<div className="col-lg-4">
+			<div className="p-3">
+				<Legend/>
+			</div>
+					</div>
 				</div>
-
+			</div>
+		</>
 	);
 }
