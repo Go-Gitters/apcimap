71ad799a35863a51cf6dcec35d44290a6121e0a1
Kyla Bendt <kylabendt@gmail.com>
2019-12-08 16:50:21 -0700

    add reducers

---
+++ app/src/shared/reducers/crimeReducer.js
@@ -0,0 +1,13 @@
+
+export default (state = [], action) => {
+	switch(action.type) {
+		case "GET_ALL_CRIMES":
+			return action.payload;
+		case "GET_CRIME_BY_CRIME_ID":
+			return [...state, action.payload];
+		case "GET_CRIME_BY_LOCATION":
+			return action.payload;
+		default:
+			return state;
+	}
+}
