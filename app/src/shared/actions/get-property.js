01d2ab82adda8999d33efd16ae0957c791bc67e0
Kyla Bendt <kylabendt@gmail.com>
2019-12-08 19:43:40 -0700

    fix some copy pasta

--- app/src/shared/actions/get-property.js
+++ app/src/shared/actions/get-property.js
@@ -2,15 +2,15 @@ import {httpConfig} from "../misc/http-config";
 
 export const getPropertyByPropertyId = (id) => async dispatch => {
 	const {data} = await httpConfig('/apis/property/?propertyId=${id}');
-	dispatch({type: "GET_CRIME_BY_CRIME_ID", payload: data })
+	dispatch({type: "GET_PROPERTY_BY_PROPERTY_ID", payload: data })
 };
 
 export const getAllProperty = () => async dispatch => {
 	const {data} = await httpConfig('/apis/property/');
-	dispatch({type: "GET_ALL_CRIME", payload: data })
+	dispatch({type: "GET_ALL_PROPERTY", payload: data })
 };
 
 export const getPropertyByPropertyLocation = (lat, long, distance) => async dispatch => {
 	const {data} = await httpConfig('/apis/property/?lat=${lat}&long=${long}&distance=${distance}');
-	dispatch({type: "GET_CRIME_BY_CRIME_LOCATION", payload: data })
+	dispatch({type: "GET_PROPERTY_BY_PROPERTY_LOCATION", payload: data })
 };
--- app/src/shared/reducers/crimeReducer.js
+++ app/src/shared/reducers/crimeReducer.js
@@ -1,7 +1,7 @@
 
 export default (state = [], action) => {
 	switch(action.type) {
-		case "GET_ALL_CRIMES":
+		case "GET_ALL_CRIME":
 			return action.payload;
 		case "GET_CRIME_BY_CRIME_ID":
 			return [...state, action.payload];
